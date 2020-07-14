2595123b21db1d4d2cc72ff0b8a0256b8aa506f8
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-07-14 10:36:47 +0200

    updates drei version

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,6 @@ import { Sphere } from "drei";
 
 import "./App.css";
 
-// function Sphere(props) {
-//   // This reference will give us direct access to the mesh
-//   const mesh = useRef();
-//   // Set up state for the hovered and active state
-//   const [hovered, setHover] = useState(false);
-//   const [active, setActive] = useState(false);
-
-//   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-
-//   return (
-//     <mesh
-//       {...props}
-//       ref={mesh}
-//       scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-//       onClick={(e) => setActive(!active)}
-//       onPointerOver={(e) => setHover(true)}
-//       onPointerOut={(e) => setHover(false)}
-//     >
-//       <sphereBufferGeometry attach="geometry" args={[1, 30, 30]} />
-//       <meshStandardMaterial
-//         attach="material"
-//         color={hovered ? "hotpink" : "teal"}
-//       />
-//     </mesh>
-//   );
-// }
-
 function Box(props) {
   // This reference will give us direct access to the mesh
   const mesh = useRef();
@@ -59,6 +32,8 @@ function Box(props) {
 }
 
 const App = () => {
+  console.log("APp");
+
   return (
     <Canvas colorManagement>
       <ambientLight />
@@ -66,9 +41,7 @@ const App = () => {
       <Box position={[1, 2, 0]} />
       <Box position={[-1, 1, 0]} />
       <Box position={[1.2, 0, 0]} />
-      <Sphere>
-        <meshBasicMaterial attach="material" color="hotpink" />
-      </Sphere>
+      <Sphere />
     </Canvas>
   );
 };
